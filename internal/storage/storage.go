d0c255acb8a16e0449ebe2ddbf3e9c1d4751f2ae
konstantiner <kastyan0403@mail.ru>
2023-03-26 09:21:38 +0300

    Изменения в app.go

--- internal/storage/storage.go
+++ internal/storage/storage.go
@@ -23,15 +23,16 @@ func (s *Service) AllUsers() map[int]entities.User{
 
 func (s *Service) CreateUser(u entities.User) int {
 	s.mutex.Lock()
+	defer s.mutex.Unlock()
 	newId := s.counterId
 	s.counterId ++
 	s.store[newId] = u
-	s.mutex.Unlock()
 	return newId
 }
 
 func (s *Service) MakeFriends(SourceId int, TargetId int) {
 	s.mutex.Lock()
+	defer s.mutex.Unlock()
 	if entry, ok := s.store[SourceId]; ok{
 		entry.Friends = append(entry.Friends, TargetId)
 		s.store[SourceId] = entry
@@ -41,17 +42,17 @@ func (s *Service) MakeFriends(SourceId int, TargetId int) {
 		entry.Friends = append(entry.Friends, SourceId)
 		s.store[TargetId] = entry
 	}
-	s.mutex.Unlock()
 }
 
 func (s *Service) DeleteUser(userId int) {
 	s.mutex.Lock()
+	defer s.mutex.Unlock()
 	delete(s.store, userId) 
-	s.mutex.Unlock()
 }
 
 func (s *Service) DeleteFriend(userId int, friendId int) {
 	s.mutex.Lock()
+	defer s.mutex.Unlock()
 	var friendsUser []int = s.store[userId].Friends
 	
 	for y, z := range friendsUser {
@@ -64,28 +65,27 @@ func (s *Service) DeleteFriend(userId int, friendId int) {
 			break
 		}
 	}
-	s.mutex.Unlock()
 }
 
 func (s *Service) UserFriends(userId int) []int {
 	s.mutex.Lock()
+	defer s.mutex.Unlock()
 	friends := s.store[userId].Friends
-	s.mutex.Unlock()
 	return friends
 }
 
 func (s *Service) UserName(userId int) string {
 	s.mutex.Lock()
+	defer s.mutex.Unlock()
 	name := s.store[userId].Name
-	s.mutex.Unlock()
 	return name
 }
 
 func (s *Service) UpdateUserAge(userId int, age int) {
 	s.mutex.Lock()
+	defer s.mutex.Unlock()
 	if entry, ok := s.store[userId]; ok{
 		entry.Age = age
 		s.store[userId] = entry
 	}
-	s.mutex.Unlock()
 }
